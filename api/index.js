8bef130a4af5fae1a6b62a5e801d87240447dde8
Ashar <muhammadashar1337@gmail.com>
2023-10-08 13:08:35 +0500

    setting up mongo and connecting to it

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://ash:ash@cluster0.da4kcd0.mongodb.net/mern-estate?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("The Server is connected to mongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("This port is working");
