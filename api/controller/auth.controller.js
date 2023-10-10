f1c8eb941c2304c86e54ce856a2a329fdf3eaec8
Ashar <muhammadashar1337@gmail.com>
2023-10-10 22:37:04 +0500

    signin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,10 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
+dotenv.config();
 //Craeating a function that uses model to create a user in a database
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,6 +13,24 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("New user was created");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
